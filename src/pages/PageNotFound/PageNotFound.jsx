d725e68f96135b8f45fd29c147b0dfc093f1e703
phanhieukhang1999 <phanhieukhang1999gmail.com>
2022-12-02 13:03:02 +0700

    upload code, page 404 lan 2

--- src/pages/PageNotFound/PageNotFound.jsx
+++ src/pages/PageNotFound/PageNotFound.jsx
@@ -10,7 +10,7 @@ export default function PageNotFound() {
                         <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
                             <span className="sr-only">Error</span>404
                         </h2>
-                        <p className="text-2xl font-semibold md:text-3xl mb-5">Xin lỗi, chúng tôi không tìm thấy trang này.</p>
+                        <p className="text-2xl font-semibold md:text-3xl mb-5">Xin lỗi, trang đang trong quá trình phát triển !</p>
                         {/* <p className="mt-4 mb-8 dark:text-gray-400">But dont worry, you can find plenty of other things on our homepage.</p> */}
                         <Link rel="noopener noreferrer" to='/' className="px-8 py-3 font-semibold text-white rounded bg-green-500 dark:bg-violet-400 dark:text-gray-900">Back to homepage</Link>
                     </div>
